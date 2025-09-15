dce238cbc223564cb38705df024697f740fabcb6
Junyeong Cha <vokdol@yonsei.ac.kr>
2025-09-15 22:38:55 +0900

    2단원 문풀 살짝 추가

---
+++ chapter2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# #5번(a)
+# x=np.array([[3,4,-1], [1,0,3], [2,5,-4]])
+# y=np.linalg.inv(x)
+# print(y)
+# print(x.dot(y))
+# 
+# #5번(b)
+# x=np.array([[1,0,1], [0,1,1], [1,1,0]])
+# y=np.linalg.inv(x)
+# print(y)
+# print(x.dot(y))
